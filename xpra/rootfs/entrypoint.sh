5ae330349c1c5af3e0ea141622d1c746b373eef8
fakuivan <fakuivan@gmail.com>
2022-03-04 17:52:02 -0300

    Set home env variable on entrypoint

--- xpra/rootfs/entrypoint.sh
+++ xpra/rootfs/entrypoint.sh
@@ -1,3 +1,2 @@
-#!/usr/bin/env sh
-cd /home/xpra_user
-exec /init s6-setuidgid xpra_user "$@"
+#!/usr/bin/env bash
+exec /init s6-setuidgid xpra_user bash -c 'export HOME=~xpra_user && cd && "$@"' -- "$@"
